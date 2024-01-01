91419f8ab4c4cd27f66252f624a9a3917cd80413
Jones-Harami <154979125+Jones-Harami@users.noreply.github.com>
2024-01-02 00:22:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -129,7 +129,7 @@ async def main(bot: Client, message: Message):
             return
 
         if user_id not in Config.BOT_OWNER:
-        await cmd.reply_text("You do not have permission to perform this action.")
+        await message.reply_text("You do not have permission to perform this action.")
         return
 
         await message.reply_text(
