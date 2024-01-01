74defba2448efdbaa989c4e311f704f58d0fdd6d
Jones-Harami <154979125+Jones-Harami@users.noreply.github.com>
2024-01-01 23:58:37 +0530

    Update add_user_to_db.py

--- handlers/add_user_to_db.py
+++ handlers/add_user_to_db.py
@@ -15,8 +15,13 @@ async def add_user_to_database(bot: Client, cmd: Message):
                 f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
             )
 
-async def only_admin_access(bot: Client, cmd: Message):
-    if Config.OTHER_USERS_CAN_SAVE_FILE is False:
-        return
-    elif cmd.from_user.id not in Config.BOT_OWNER:
+async def only_admin_access(bot: Client, cmd: Message, editable: Message):
+    user_id = cmd.from_user.id
+
+    # Check if user has admin access and file-saving permission
+    if not Config.OTHER_USERS_CAN_SAVE_FILE or user_id not in Config.BOT_OWNER:
+        if not Config.OTHER_USERS_CAN_SAVE_FILE:
+            await editable.edit("You do not have permission to save files.")
+        elif user_id not in Config.BOT_OWNER:
+            await editable.edit("You do not have permission to perform this action.")
         return
